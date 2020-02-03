1c98664022e1e1d244f110d9be9c0b2f739b844d
reguv760 <reguv760@gmail.com>
2020-02-03 11:49:03 -0800

    init repo: part 1 tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { TweenMax, Power3 } from "gsap";
 
 function App() {
+  let logoImg = useRef(null);
+  let textItem = useRef(null);
+
+  useEffect(() => {
+    console.log(logoImg);
+    TweenMax.to(logoImg, 0.8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut
+    });
+
+    TweenMax.to(textItem, 0.8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut,
+      delay: 0.2
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src={logo}
+          className='App-logo'
+          alt='logo'
+          ref={el => {
+            logoImg = el;
+          }}
+        />
+        <p
+          ref={el => {
+            textItem = el;
+          }}
+        >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
